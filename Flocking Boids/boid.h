9e15ff1c0cfd0bcc4aba655ebac307cce90ae6d2
Roozbeh <roozbehsayadi@gmail.com>
2020-12-09 22:43:50 +0330

    Added empty classes.

---
+++ Flocking Boids/boid.h
@@ -0,0 +1,10 @@
+
+#ifndef __BOID_H
+#define __BOID_H
+
+// Each boid in a flock is represented by this class.
+class Boid {
+};
+
+#endif 
+
---
+++ Flocking Boids/flock.h
@@ -0,0 +1,11 @@
+
+#ifndef __FLOCK_H
+#define __FLOCK_H
+
+// Each flock consists of some boids. Creates and
+// holds one flock and handles their movements.
+class Flock {
+};
+
+#endif
+
