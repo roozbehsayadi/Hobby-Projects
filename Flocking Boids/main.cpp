d36fff0604187070e3d771825ec1a1d2abbe25a2
Roozbeh <roozbehsayadi@gmail.com>
2020-12-11 01:31:11 +0330

    Handled the situation where the user enters a string instead of a number in running the code.

--- Flocking Boids/main.cpp
+++ Flocking Boids/main.cpp
@@ -10,6 +10,10 @@ int main ( int argc, char **argv ) {
 		std::cout << "Usage:\n./Flocking Number_of_Boids" << std::endl;
 		return 1;
 	}
+	if ( atoi( argv[1] ) == 0 ) {
+		std::cout << "Number of boids shoud be more that zero\nOr maybe you entered a string instead of a number...?" << std::endl;
+		return 1;
+	}
 
 	Flock flock;
 	flock.initialize( atoi( argv[1] ) );
