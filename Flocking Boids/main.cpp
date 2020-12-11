237b90461c0ed0b5f5d6d705797d11dea094dda0
Roozbeh <roozbehsayadi@gmail.com>
2020-12-12 01:40:27 +0330

    Changed the behaviour of main parameters. Now you have to provide number of flocks and total number of boids when running the program.

--- Flocking Boids/main.cpp
+++ Flocking Boids/main.cpp
@@ -6,23 +6,38 @@
 
 #include "flockingBoids.h"
 
+Color colors[] = { {255, 0, 0 }, {255, 127, 0}, {255, 255, 0}, {0, 255, 0}, {0, 255, 255}, {0, 0, 255}, {255, 0, 255} };
+
 int main ( int argc, char **argv ) {
 
-/*	if ( argc != 2 ) {
-		std::cout << "Usage:\n./Flocking Number_of_Boids" << std::endl;
+	if ( argc != 3 ) {
+		std::cout << "Usage:\n./Flocking Number_of_Flocks Total_Number_of_Boids" << std::endl;
 		return 1;
 	}
-	if ( atoi( argv[1] ) == 0 ) {
-		std::cout << "Number of boids shoud be more that zero\nOr maybe you entered a string instead of a number...?" << std::endl;
+
+	int flocksCount = atoi( argv[1] );
+	int totalBoidsCount = atoi( argv[2] );
+
+	if ( flocksCount <= 0 ) {
+		std::cout << "Number of flocks shoud be more that zero\nOr maybe you entered a string instead of a number...?" << std::endl;
 		return 1;
-	}*/
+	}
+
+	if ( totalBoidsCount <= 0 ) {
+		std::cout << "Total number of boids shoud be more that zero\nOr maybe you entered a string instead of a number...?" << std::endl;
+		return 1;
+	}
+
+	if ( flocksCount > 7 ) {
+		std::cout << "Sorry, we only have 7 colors available for now. :)\n" << std::endl;
+		return 1;
+	}
 
 	srand( time( nullptr ) );
 
-	// FlockingBoids flockingBoids( atoi( argv[1] ), -1, -1 );
 	FlockingBoids flockingBoids( -1, -1 );
-	flockingBoids.addFlock( 50, {0, 255, 255} );
-	flockingBoids.addFlock( 50, {255, 255, 0} );
+	for ( int i = 0; i < flocksCount; i++ )
+		flockingBoids.addFlock( totalBoidsCount / flocksCount, colors[ i ] );
 	flockingBoids.start();
 
 	return 0;
